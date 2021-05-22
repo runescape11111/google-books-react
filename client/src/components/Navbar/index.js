4bacd9c7556d16d7e3b7e2dc33074acc501fdecf
Oliver Shih <olivershih@gmail.com>
2021-05-21 21:56:52 -0700

    dried up navbar and navlinks

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,22 +1,26 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
+
+function NavItem({to, children}) {
+    const {pathname} = useLocation();
+    return (
+        <li className="nav-item px-2">
+            <NavLink className="nav-link" exact to={to}>
+                {children} {" "}
+                {pathname === to && <span className="sr-only">(current)</span>}
+            </NavLink>
+        </li>
+    )
+}
 
 function Navbar() {
     return (
         <nav className="navbar navbar-dark bg-dark">
-            <Link className="navbar-brand" to="/">
+            <Link className="navbar-brand" exact to="/">
                 Google Books React
             </Link>
             <ul className="navbar-nav flex-row">
-                <li className="nav-item px-2">
-                    <NavLink className="nav-link" exact to="/">
-                        Search
-                    </NavLink>
-                </li>
-                <li className="nav-item px-2">
-                    <NavLink className="nav-link" exact to="/saved">
-                        Saved
-                    </NavLink>
-                </li>
+                <NavItem to="/">Search</NavItem>
+                <NavItem to="/saved">Saved</NavItem>
             </ul>
         </nav>
     )
