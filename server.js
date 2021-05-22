ec191f26f117e45a73c1c2a4198d7d1d13a90f6e
Oliver Shih <olivershih@gmail.com>
2021-05-21 20:14:17 -0700

    changed file and directory name

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+mongoose.connect("mongodb://localhost/google_books_db", {useNewUrlParser: true, useUnifiedTopology: true});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
