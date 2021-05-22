dfbad093e6d3e5f177d2bc0ebe88f9a8cb0f0740
Oliver Shih <olivershih@gmail.com>
2021-05-21 21:41:25 -0700

    navbar and routing finished

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,6 +1,24 @@
+import { Link, NavLink } from "react-router-dom";
+
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <nav className="navbar navbar-dark bg-dark">
+            <Link className="navbar-brand" to="/">
+                Google Books React
+            </Link>
+            <ul className="navbar-nav flex-row">
+                <li className="nav-item px-2">
+                    <NavLink className="nav-link" exact to="/">
+                        Search
+                    </NavLink>
+                </li>
+                <li className="nav-item px-2">
+                    <NavLink className="nav-link" exact to="/saved">
+                        Saved
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
     )
 };
 
