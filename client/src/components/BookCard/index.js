5aed601c300f601acc24c38d5f65d96de08fdbf5
Oliver Shih <olivershih@gmail.com>
2021-05-23 13:17:04 -0700

    made book card with fake data

--- client/src/components/BookCard/index.js
+++ client/src/components/BookCard/index.js
@@ -1,7 +1,42 @@
-function BookCard() {
+function BookCard({book}) {
+    const vol = book.volumeInfo;
+    console.log(vol);
+    const authors = vol.authors.join(", ");
     return (
-        <div>
-            BookCard
+        <div className="card bg-dark mx-3 my-3 border-light">
+            <div className="card-header text-light border-light">
+                <h3 className="card-title">
+                    {vol.title}
+                </h3>
+                <div className="card-subtitle">
+                    Written by {authors}
+                </div>
+            </div>
+            <div className="card-text container-fluid">
+                <div className="row">
+                    <div className="col-12 col-sm-3 col-md-2 py-3">
+                        <img className="img-responsive w-100" src={vol.imageLinks.thumbnail} alt={vol.title} />
+                    </div>
+                    <div className="col-12 col-sm-9 col-md-10 py-3">
+                        <div className="row">
+                            <div className="col-12">
+                                <h4 className="text-light">
+                                    Description
+                                </h4>
+                            </div>
+                            <div className="col-12">
+                                <p className="text-light">
+                                    {vol.description}
+                                </p>
+                            </div>
+                            <div className="col-12 d-flex justify-content-end">
+                                <button className="btn btn-success ml-4 px-4">Save</button>
+                                <a href={vol.infoLink} target="_blank" rel="noreferrer" className="btn btn-light ml-4 px-4">View</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -1,13 +1,16 @@
 import BookCard from "../BookCard";
 import CardHeader from "../CardHeader";
 
+import fakeData from "../../fakeData.json";
+const fakeSearchResults = fakeData.items;
+
 function BookList() {
     return (
         <div className="card bg-dark mx-5">
             <CardHeader>Results</CardHeader>
-            <BookCard />
-            <BookCard />
-            <BookCard />
+            {fakeSearchResults.map(result => {
+                return (<BookCard key={result.id} book={result}/>)
+            })}
         </div>
     )
 };
